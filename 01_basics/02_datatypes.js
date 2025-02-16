f8b9fd8479ce5f5140dd211881e53c53cb03ede7
prt-04224 <tripradyuman54@gmail.com>
2025-02-16 09:53:01 +0000

    commit

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,29 @@
+
+
+// alert( 3 + 3) // we are using nodejs, not browser
+
+console.log(3+3) // code readability should be high
+
+console.log("youVA")
+
+
+let name = "neel"
+let age = 18
+let isLoggedIn = false
+let state;
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+
+// object
+let DMC=123
+console.log(typeof DMC);
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
+console.log(typeof name);
