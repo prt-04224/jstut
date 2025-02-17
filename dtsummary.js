26528cef13685e7fbb8d24cf35483db81c06caa1
prt-04224 <tripradyuman54@gmail.com>
2025-02-17 13:30:18 +0000

    dts

---
+++ dtsummary.js
@@ -0,0 +1,13 @@
+//Primitive
+//call by value
+//7 types
+//String
+//Number,Boolean,null
+//undefined,Symbol
+//BigInt
+
+
+//Non Primitive(Reference type)
+//Memory reference is directly allocated;
+
+//Array,Objects,Functions
