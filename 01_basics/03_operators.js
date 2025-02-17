ee988ff58f06db4030b233d3cd0f1230ea6b0a8f
prt-04224 <tripradyuman54@gmail.com>
2025-02-17 12:36:11 +0000

    commit

--- 01_basics/03_operators.js
+++ 01_basics/03_operators.js
@@ -1,21 +1,24 @@
-let score="33"
-console.log(typeof score);
-console.log(score);
-let inn = Number(score)
-console.log(typeof inn);
-console.log(inn)
-// "33" => 33
-// "33abc" => NaN
-// true => 1; false=>0
+// let score="33"
+// console.log(typeof score);
+// console.log(score);
+// let inn = Number(score)
+// console.log(typeof inn);
+// console.log(inn)
+// // "33" => 33
+// // "33abc" => NaN
+// // true => 1; false=>0
 
-let isLoggedIn = ""
+// let isLoggedIn = ""
 
-let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn)
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn)
 // 1 => true; 0=>false;
 // "" => false
 // "string" => true
-let ser=456;
-let numser=String(ser);
-console.log(numser)
-console.log(typeof(numser))
+//let ser=456;
+//let numser=String(ser);
+//console.log(numser)
+//console.log(typeof(numser))
+// let gamecounter = 100
+// ++gamecounter;
+// console.log(gamecounter);
